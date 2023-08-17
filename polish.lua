1f260cb93e9a49a06240268ff5f11aaecedb789d
NguyenTrongTin1107 <ntrongtin11702@gmail.com>
2023-08-17 14:44:10 +0700

    fix: read yaml file

--- polish.lua
+++ polish.lua
@@ -5,10 +5,10 @@ return function()
 
     -- check if file is in roles, tasks, or handlers folder
     local path_regex = vim.regex "(tasks\\|roles\\|handlers)/"
-    if path_regex and path_regex:match_str(path) then return "yaml.ansible" end
+    if path ~= nil and path_regex and path_regex:match_str(path) then return "yaml.ansible" end
     -- check for known ansible playbook text and if found, return yaml.ansible
     local regex = vim.regex "hosts:\\|tasks:"
-    if regex and regex:match_str(content) then return "yaml.ansible" end
+    if content ~= nil and regex and regex:match_str(content) then return "yaml.ansible" end
 
     -- return yaml if nothing else
     return "yaml"
