0b783784d1a4aa0e0348086cb9c3d103b3dca53b
NguyenTrongTin <ntrongtin11702@gmail.com>
2023-12-20 13:04:01 +0700

    feat: remove octo

--- highlights/init.lua
+++ highlights/init.lua
@@ -5,6 +5,5 @@ return function()
     CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
     GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true }, -- italicize git blame virtual text
     HighlightURL = { underline = true }, -- always underline URLs
-    OctoEditable = { fg = "NONE", bg = "NONE" }, -- use treesitter for octo.nvim highlighting
   }
 end
