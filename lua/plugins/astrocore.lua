a3a2e403fcccb7eacc0c2a472420b7257aa7e417
NguyenTrongTin <ntrongtin11702@gmail.com>
2024-04-01 22:56:15 +0700

    fix: switch to normal mode in terminal

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -72,6 +72,7 @@ return {
       t = {
         -- setting a mapping to false will disable it
         -- ["<esc>"] = false,
+        ["<C-n>"] = { "<C-\\><C-n>", desc = "Terminal normal mode" },
       },
     },
   },
